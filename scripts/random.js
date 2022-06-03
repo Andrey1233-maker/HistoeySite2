bd87f0f2bcddb987dce9df42d5c2e55849967bcc
Andrey1233-maker <andrey1mosckalenko@gmail.com>
2022-06-03 23:32:08 +0300

    big kek

---
+++ scripts/random.js
@@ -0,0 +1,20 @@
+
+function randomState(){
+    const index = Math.floor(Math.random() * pageArray.length)
+    return location.href = pageArray[index]
+}
+
+const pageArray = ["adolf.html",
+"albert.html",
+"aristotel.html",
+"bolt.html",
+"cleopatra.html",
+"columb.html",
+"darwin.html",
+"ervin.html",
+"gerodot.html",
+"nuton.html",
+"petr.html",
+"rurik.html",
+"shteiner.html",
+]
